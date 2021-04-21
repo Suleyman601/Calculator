119cecc3d9abd0181283da532e628e0c79011b2f
Suleyman601 <suleymanjama.32@gmail.com>
2021-04-21 16:22:44 -0700

    Only needs responsiveness. All Functionality works

---
+++ main.js
@@ -0,0 +1,216 @@
+class multiply{
+  constructor(){
+    this.precedence = 1;
+    this.numOfArg = 2;
+  }
+  
+  perform(args){
+    return args[1] * args[0];
+  }
+
+}
+
+class subtract{
+  constructor(){
+    this.precedence = 0;
+    this.numOfArg = 2;
+  }
+  
+  perform(args){
+    return args[1] - args[0];
+  }
+}
+
+class divide{
+  constructor(){
+    this.precedence = 1;
+    this.numOfArg = 2;
+  }
+  
+  perform(args){
+    if(args[0] === 0){
+      return false;
+    }
+    return args[1] / args[0];
+  }
+}
+
+class add{
+  constructor(){
+    this.precedence = 0;
+    this.numOfArg = 2;
+  }
+  
+  perform(args){
+    return args[1] + args[0];
+  }
+}
+
+// class negation{
+//   constructor(){
+//     this.precedence = 3;
+//     this.numOfArg = 1;
+//   }
+  
+//   perform(a){
+//     return a * -1;
+//   }
+// }
+
+// class positive{
+//   constructor(){
+//     this.precedence = 3;
+//     this.numOfArg = 1;
+//   }
+  
+//   perform(a){
+//     return a;
+//   }
+// }
+
+function outputToScreen(e){
+  const screen = document.querySelector('#display p');
+  const regex = /(\d+\.\d+|\d+\.|\d+|-|\+|x|÷)/g;
+  let str = screen.innerText;
+  let found = str.match(regex);
+  if(e.target.value === '.' && (str === '' || str.charAt(str.length -1) == '\u00A0')){
+    return;
+  }
+  if(e.target.value === "." && str!='' && found[found.length-1].includes('.')){
+    return;
+  } 
+  if(e.target.value === "+" || e.target.value === "-" || e.target.value === "x" || e.target.value === "÷"){
+    if(str == ''){
+      return;
+    }
+    if(found[found.length -1] === "+" || found[found.length -1] === "-" || found[found.length -1] === "÷" || found[found.length -1] === "x"){
+      return;
+    }
+    
+  }
+  if(e.target.value === "+" || e.target.value === "-" || e.target.value === "x" || e.target.value === "÷"){
+    screen.innerText += '\u00A0';
+    screen.innerText += e.target.value;
+    screen.innerText += '\u00A0';
+  }
+  else{
+    screen.innerText += e.target.value;
+  }
+}
+
+function clear(){
+  const screen = document.querySelector('#display p');
+  screen.innerText = '';
+}
+
+function deleteLast(){
+  const screen = document.querySelector('#display p');
+  const str = screen.innerText;
+  screen.innerText = str.substring(0,str.length -1);
+}
+
+function operator(str){
+  if(str === '+')
+    return new add();
+  else if(str === '-')
+    return new subtract();
+  else if(str === '÷')
+    return new divide();
+  else if(str === 'x')
+    return new multiply();
+}
+
+function lexer(str){
+  const regex = /(\d+\.\d+|\d+|-|\+|x|÷)/g;
+  let found = str.match(regex);
+  for (let i = 0; i < found.length; i++) {
+    if(isNaN(+found[i])){
+      found[i] = operator(found[i])
+    }
+    else{
+      found[i] = +found[i];
+    }
+  }
+  return found;
+}
+
+function parser(){
+  const screen = document.querySelector('#display p');
+  let str = screen.innerText;
+  let tokenArr = lexer(str);
+  let queue = [];
+  let stack = [];
+  for (let i = 0; i < tokenArr.length; i++) {
+    if(!isNaN(tokenArr[i])){
+      queue.push(tokenArr[i]);
+    }
+    else {
+      const op = tokenArr[i];
+      while(stack.length != 0 && stack[stack.length-1].precedence >= op.precedence ){
+        queue.push(stack.pop());
+      }
+      stack.push(op);
+    }
+  }
+  while(stack.length != 0){
+    queue.push(stack.pop());
+  }
+  return queue;
+}
+
+function evaluate(){
+  const screen = document.querySelector('#display p');
+  let str = screen.innerText;
+  const regex = /(\d+\.\d+|\d+|-|\+|x|÷)/g;
+  let found = str.match(regex);
+  if(str === ''){
+    return;
+  }
+
+  if(found.length === 1){
+    return;
+  }
+
+  if(found[found.length -1] === '+'|| found[found.length -1] === '-'|| found[found.length -1] === '÷'|| found[found.length -1] === 'x'){
+    return;
+  }
+  let queue = parser();
+  let stack = [];
+  for (let i = 0; i < queue.length; i++) {
+    if(!isNaN(queue[i])){
+      stack.push(queue[i]);
+    }
+    else{
+      let args = [];
+      console.log(queue[i].numOfArg);
+      for (let j = 0; j < queue[i].numOfArg; j++) {
+        args.push(stack.pop());
+      }
+      let result = queue[i].perform(args);
+      if(result === false){
+        return;
+      }
+      stack.push(result);
+    }
+  }
+  screen.innerText = stack.pop();
+}
+
+
+
+
+
+const btns = document.querySelectorAll(".btn");
+
+btns.item(0).addEventListener('click', clear);
+btns.item(1).addEventListener('click', deleteLast);
+for(let i = 2; i < btns.length - 1; ++i){
+  btns.item(i).addEventListener('click', outputToScreen);
+}
+
+btns.item(btns.length-1).addEventListener('click', evaluate);
+
+
+//to do
+//handle when paragraph to full
+//handle number 
